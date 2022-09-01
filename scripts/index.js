fabac63be82f8dd70a41e016da97b666a3153a74
sayyaa <iammosin.d@gmail.com>
2022-09-01 22:36:31 +0300

    feat: add second popup html | popup open\close function and like function

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,41 @@
-const popup = document.querySelector('.popup');
+const popup = document.querySelectorAll('.popup');
 const editButton = document.querySelector('.hero__edit');
-const closeButton = document.querySelector('.popup__close-btn');
+const addButton = document.querySelector('.hero__add');
+const likeButton = document.querySelectorAll('.content__like');
+const closeButton = document.querySelectorAll('.popup__close-btn');
 const popupForm = document.querySelector('.popup__form')
 const inputName = document.querySelector('.popup__form-str_text_name');
 const inputDescription = document.querySelector('.popup__form-str_text_occupation');
 const heroName = document.querySelector('.hero__name');
 const heroDescription = document.querySelector('.hero__description');
 
-const popupOpen = () => {
-  inputName.value = heroName.textContent;
-  inputDescription.value = heroDescription.textContent;
-  popup.classList.add('popup__opened');
+const popupOpen = (event) => {
+  if (event.target.classList.contains('hero__edit')) {
+    inputName.value = heroName.textContent;
+    inputDescription.value = heroDescription.textContent;
+    popup[0].classList.add('popup__opened');
+  } else if (event.target.classList.contains('hero__add')) {
+      popup[1].classList.add('popup__opened');
+  }
 }
 
 const popupClose = () => {
-  popup.classList.remove('popup__opened')
+  popup.forEach((el) => el.classList.remove('popup__opened'));
 }
 
-const changeProfile = (evt) => {
-  evt.preventDefault();
+const changeProfile = (event) => {
+  event.preventDefault();
   heroName.textContent = inputName.value;
   heroDescription.textContent = inputDescription.value;
   popupClose();
 }
 
-editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupClose);
-popupForm.addEventListener('submit', changeProfile);
+const putLike = (event) => {
+  event.target.classList.toggle('content__like_active');
+}
 
+editButton.addEventListener('click', popupOpen);
+addButton.addEventListener('click', popupOpen);
+likeButton.forEach(el => el.addEventListener('click', putLike));
+closeButton.forEach((el) => el.addEventListener('click', popupClose))
+popupForm.addEventListener('submit', changeProfile);
