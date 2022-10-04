9e983372d59bd4c6e26caab0bd57d6ac2e0f5de4
sayyaa <iammosin.d@gmail.com>
2022-10-04 20:20:42 +0300

    fix: before second review

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -2,30 +2,31 @@ class FormValidator {
   constructor(obj, form) {
     this._obj = obj;
     this._form = form;
+    this._inputs = [...this._form.querySelectorAll(this._obj.inputSelector)];
+    this._button = this._form.querySelector(this._obj.submitButtonSelector);
   }
 
-  // функция, показывающая сообщение об ошибке, когда поле становится не валидным
-
-  _findVariableError(input) {
-    const error = this._form.querySelector(`.${input.id}-error`);
-    return error;
-  }
+  // метод, показывающий сообщение об ошибке, когда поле становится не валидным
 
   _showInputError(input, errorMessage) {
-    this._findVariableError(input).textContent = errorMessage;
-    this._findVariableError(input).classList.add(this._obj.errorClass);
+    const error = this._form.querySelector(`.${input.id}-error`);
+
+    error.textContent = errorMessage;
+    error.classList.add(this._obj.errorClass);
     input.classList.add(this._obj.inputErrorClass);
   }
 
-  // функция, скрывающая сообщение об ошибке, когда поле становится валидным
+  // метод, скрывающий сообщение об ошибке, когда поле становится валидным
 
   _hideInputError(input) {
-    this._findVariableError(input).classList.remove(this._obj.errorClass);
+    const error = this._form.querySelector(`.${input.id}-error`);
+
+    error.classList.remove(this._obj.errorClass);
     input.classList.remove(this._obj.inputErrorClass);
-    this._findVariableError(input).textContent = "";
+    error.textContent = "";
   }
 
-  // функция, проверяющая инпут на валидность
+  // метод, проверяющий инпут на валидность
 
   _checkInputValidity(input) {
     if (!input.validity.valid) {
@@ -35,48 +36,44 @@ class FormValidator {
     }
   }
 
-  // функция, проверяющая есть ли поле не прошедшее валидацию, если возратит true значит невалид
+  // метод, проверяющий есть ли поле не прошедшее валидацию, если возратит true значит невалид
 
-  _hasInvalidInput(inputs) {
-    return inputs.some((input) => !input.validity.valid);
+  _hasInvalidInput() {
+    return this._inputs.some((input) => !input.validity.valid);
   }
 
-  // функция делающая кнопку неактивной если поле невалидно
+  // метод делающий кнопку неактивной если поле невалидно
 
-  _toggleButtonState = (inputs, button) => {
-    if (this._hasInvalidInput(inputs)) {
-      button.classList.add(this._obj.inactiveButtonClass);
-      button.setAttribute("disabled", true);
+  _toggleButtonState = () => {
+    if (this._hasInvalidInput()) {
+      this._button.classList.add(this._obj.inactiveButtonClass);
+      this._button.setAttribute("disabled", true);
     } else {
-      button.classList.remove(this._obj.inactiveButtonClass);
-      button.removeAttribute("disabled", false);
+      this._button.classList.remove(this._obj.inactiveButtonClass);
+      this._button.removeAttribute("disabled", false);
     }
   };
-  // функция устанавливающая обработчик на поля
+  // метод устанавливающий обработчик на поля
 
   enableValidation() {
-    const inputs = [...this._form.querySelectorAll(this._obj.inputSelector)];
-    const button = this._form.querySelector(this._obj.submitButtonSelector);
 
-    this._toggleButtonState(inputs, button);
+    this._toggleButtonState();
 
-    inputs.forEach((input) => {
+    this._inputs.forEach((input) => {
       input.addEventListener("input", () => {
         this._checkInputValidity(input);
-        this._toggleButtonState(inputs, button);
+        this._toggleButtonState();
       });
     });
   }
 
   disableValidation() {
-    const inputs = [...this._form.querySelectorAll(this._obj.inputSelector)];
-    const button = this._form.querySelector(this._obj.submitButtonSelector);
 
-    inputs.forEach((input) => {
+    this._inputs.forEach((input) => {
       this._hideInputError(input);
     });
 
-    this._toggleButtonState(inputs, button);
+    this._toggleButtonState();
   }
 }
 
